7d1fd51324304dc22ecbfeda2ad6161696286b5d
Dominic Cousins <dominic.cousins@gmail.com>
2018-09-05 14:20:33 +0100

    Initial commit

---
+++ src/easyrestore/EasyRestore.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package easyrestore;
+
+/**
+ *
+ * @author dominic.cousins
+ */
+public class EasyRestore {
+
+    private String installDir;
+    private String homeDir;
+    private String restoreDir;
+    private String backupDir;
+    
+    private String dbIP = "localhost:5432";
+    private String dbName = "bitbucket";
+    private String dbAccount = "bitbucketuser";
+    private String dbPassword = "CH4NG3M3";
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
